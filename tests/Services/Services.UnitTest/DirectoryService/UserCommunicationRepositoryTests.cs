998e0903d636cad761d8bc8acd7f1876f6d966d7
Adem Veli Kesim <48206386+ademkesim@users.noreply.github.com>
2023-08-07 23:01:24 +0300

    Added new tests

---
+++ tests/Services/Services.UnitTest/DirectoryService/UserCommunicationRepositoryTests.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using DirectoryService.Api.Core.Application.Repository;
+using DirectoryService.Api.Core.Domain.Concrete;
+using DirectoryService.Api.Core.Domain.Concrete.RequestDTO;
+using MongoDB.Driver;
+using DirectoryService.Api.Infrastructure.Repository;
+using DirectoryService.Api.Core.Enums;
+
+namespace Services.UnitTest.DirectoryService
+{
+    [TestClass]
+    public class UserCommunicationRepositoryTests
+    {
+        private Mock<ILoggerFactory> loggerFactoryMock;
+        private Mock<ILogger<UserCommunicationRepository>> loggerMock;
+        private Mock<IOptions<MongoDbSettings>> optionsMock;
+        private Mock<IMongoCollection<UserCommunicationInfo>> userCommunicationCollectionMock;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            loggerFactoryMock = new Mock<ILoggerFactory>();
+            loggerMock = new Mock<ILogger<UserCommunicationRepository>>();
+            userCommunicationCollectionMock = new Mock<IMongoCollection<UserCommunicationInfo>>();
+            loggerFactoryMock.Setup(factory => factory.CreateLogger(It.IsAny<string>())).Returns(loggerMock.Object);
+            optionsMock = new Mock<IOptions<MongoDbSettings>>();
+            optionsMock.Setup(options => options.Value).Returns(new MongoDbSettings()
+            {
+                ConnectionString = "mongodb://localhost:27017/",
+                Database = "telephone_directory"
+            });
+        }
+
+        [TestMethod]
+        public async Task AddUserCommunicationAsync_ShouldAddCommunicationInfo()
+        {
+            userCommunicationCollectionMock
+                .Setup(col => col.InsertOneAsync(It.IsAny<UserCommunicationInfo>(), null, default))
+                .Returns(Task.CompletedTask);
+
+            var repository = new UserCommunicationRepository(loggerFactoryMock.Object, optionsMock.Object);
+
+            var request = new AddUserCommunicationRequestDTO
+            {
+                UserInfoId = Guid.NewGuid(),
+                CommunicationType = CommunicationTypeEnum.Email,
+                CommunicationInfo = "adem@gmail.com"
+            };
+
+            // Act
+            var result = await repository.AddUserCommunicationAsync(request);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(request.UserInfoId, result.UserInfoId);
+            Assert.AreEqual(request.CommunicationType, result.CommunicationType);
+            Assert.AreEqual(request.CommunicationInfo, result.CommunicationInfo);
+        }
+
+        [TestMethod]
+        public void CheckUserCommunication_ShouldReturnTrueIfExist()
+        {
+            // Arrange
+
+            var repository = new UserCommunicationRepository(loggerFactoryMock.Object, optionsMock.Object);
+
+            var idToCheck = repository.GetUserCommunications().FirstOrDefault().Id;
+
+            // Act
+            var result = repository.CheckUserCommunication(idToCheck);
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.IsInstanceOfType(result, typeof(Boolean));
+        }
+
+        [TestMethod]
+        public async Task DeleteUserCommunicationAsync_ShouldDeleteCommunicationInfo()
+        {
+            // Arrange
+
+            var repository = new UserCommunicationRepository(loggerFactoryMock.Object, optionsMock.Object);
+
+            var idToDelete = repository.GetUserCommunications().FirstOrDefault().Id;
+
+            // Act
+            var result = await repository.DeleteUserCommunicationAsync(idToDelete);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(UserCommunicationInfo));
+        }
+    }
+}
---
+++ tests/Services/Services.UnitTest/DirectoryService/ValidateEnumAttributeTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using DirectoryService.Api.Core.Attributes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Services.UnitTest
+{
+
+    [TestClass]
+    public class ValidateEnumAttributeTests
+    {
+        private class SampleModel
+        {
+            public SampleEnum EnumProperty { get; set; }
+        }
+
+        private enum SampleEnum
+        {
+            Value1,
+            Value2,
+            Value3
+        }
+
+        private class TestController : Controller
+        {
+            [ValidateEnum(typeof(SampleEnum))]
+            public IActionResult TestAction([FromBody] SampleModel model)
+            {
+                return Ok();
+            }
+        }
+
+        [TestMethod]
+        public void EnumValueIsValid_ReturnsOkResult()
+        {
+            // Arrange
+            var attribute = new ValidateEnumAttribute(typeof(SampleEnum));
+            var actionContext = new ActionContext(
+                new DefaultHttpContext(),
+                new RouteData(),
+                new ActionDescriptor());
+            var context = new ActionExecutingContext(
+                actionContext,
+                new List<IFilterMetadata>(),
+                new Dictionary<string, object> { ["model"] = new SampleModel { EnumProperty = SampleEnum.Value1 } },
+                new TestController());
+
+            // Act
+            attribute.OnActionExecuting(context);
+
+            // Assert
+            Assert.IsNull(context.Result);
+        }
+
+        [TestMethod]
+        public void EnumValueIsInvalid_ReturnsBadRequest()
+        {
+            // Arrange
+            var attribute = new ValidateEnumAttribute(typeof(SampleEnum));
+            var actionContext = new ActionContext(
+                new DefaultHttpContext(),
+                new RouteData(),
+                new ActionDescriptor());
+            var context = new ActionExecutingContext(
+                actionContext,
+                new List<IFilterMetadata>(),
+                new Dictionary<string, object> { ["model"] = new SampleModel { EnumProperty = (SampleEnum)99 } },
+                new TestController());
+
+            // Act
+            attribute.OnActionExecuting(context);
+
+            // Assert
+            Assert.IsNotNull(context.Result);
+            Assert.IsInstanceOfType(context.Result, typeof(BadRequestObjectResult));
+            var resultValue = (BadRequestObjectResult)context.Result;
+            Assert.AreEqual("Invalid value for model.EnumProperty.", resultValue.Value);
+        }
+    }
+
+}
+
